014a5b3c9f7d0d604b79dc53b8f3b871ed7eee7f
Fauzan <sopiahsopi01@gmail.com>
2026-07-21 13:12:16 +0700

    login

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login Admin</h2>
+    <form action="proses_login.php" method="POST">
+        <label>Password</label>
+    <br>
+    <input type="password" name="password">
+    <br><br>
+    <button type="submit" name="login">
+        Login
+    </button>
+    </form>
+</body>
+</html>
